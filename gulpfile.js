a9f98219014be35622b33817b4092f63fa186d0a
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-01-17 13:21:05 +0000

    istanbul browserify

--- gulpfile.js
+++ gulpfile.js
@@ -19,3 +19,9 @@ gulp.task('debug', shell.task('NODE_PATH=./src node --inspect --debug-brk src/ma
 gulp.task("test", shell.task("NODE_PATH=./src ./node_modules/mocha/bin/mocha --require should"));
 
 gulp.task("cover", shell.task("NODE_PATH=./src ./node_modules/.bin/nyc --reporter=html ./node_modules/.bin/mocha --require should"));
+
+gulp.task("browserify", shell.task("NODE_PATH=./src browserify src/main.js -o src/bundle.js --debug"));
+
+gulp.task("watchify", shell.task("NODE_PATH=./src watchify src/main.js -o src/bundle.js --debug"));
+
+gulp.task("beefy", ["browserify"], shell.task("NODE_PATH=./src beefy src/bundle.js"));
